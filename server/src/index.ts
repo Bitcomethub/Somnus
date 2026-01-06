8c0fe44e71784a9fb237893abfb306d47c7a6d33
MacBook <macbook@MacBook-Air.local>
2026-01-06 03:30:03 +0300

    chore(branding): Rename root API response to Somnus

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.get('/', (req, res) => {
-    res.send('Tingle API: Safe Haven Active 🌙');
+    res.send('Somnus API: Safe Haven Active 🌑');
 });
 
 // Users
